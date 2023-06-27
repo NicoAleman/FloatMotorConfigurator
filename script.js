c9474afd2fc5db92ef2b902f86d38556c56220e8
Nico Aleman <NicoAleman1@hotmail.com>
2023-06-27 02:30:55 -0400

    Clear Config functionality + misc.
    - Clear Configuration now functional
    - Tweaked format of value names for motors/batteries
    - Added battery options
    - JS clean-up

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-// Function to apply colored background to specified text fields / selects
-function applyColorChangeRule(inputId, color, dimColor) {
-  const inputElement = document.getElementById(inputId);
-
-  // Set the initial background color
-  inputElement.style.backgroundColor = color;
-
-  // Check if the input element is a select
-  if (inputElement.tagName === 'SELECT') {
-    inputElement.addEventListener('change', function() {
-      // Check if the select option value is empty
-      const selectedOption = inputElement.options[inputElement.selectedIndex];
-      if (selectedOption.value === '') {
-        inputElement.style.backgroundColor = color;
-      } else {
-        inputElement.style.backgroundColor = dimColor;
-      }
-    });
-  } else {
-    inputElement.addEventListener('input', function() {
-      // Check if the input field is empty
-      if (inputElement.value === '') {
-        inputElement.style.backgroundColor = color;
-      } else {
-        inputElement.style.backgroundColor = dimColor;
-      }
-    });
-  }
-}
+// Text Field / Select background colors
+const everySetupRed = '#ff000033'
+const everySetupRedDim = '#ff000014'
+const initialSetupYellow = '#ffff0046'
+const initialSetupYellowDim = '#ffff0020'
 
 // Array of parameter IDs that must be set after every Motor Setup
 const everySetupIds = [
@@ -56,12 +32,78 @@ const initialSetupIds = [
   'bms.type'
 ];
 
+// Function to apply colored background to specified text fields / selects
+function applyParamColorRule(inputId, color, dimColor) {
+  const inputElement = document.getElementById(inputId);
+
+  // Set the initial background color
+  inputElement.style.backgroundColor = color;
+
+  // Check if the input element is a select
+  if (inputElement.tagName === 'SELECT') {
+    inputElement.addEventListener('change', function() {
+      // Check if the select option value is empty
+      const selectedOption = inputElement.options[inputElement.selectedIndex];
+      if (selectedOption.value === '') {
+        inputElement.style.backgroundColor = color;
+      } else {
+        inputElement.style.backgroundColor = dimColor;
+      }
+    });
+  } else {
+    inputElement.addEventListener('input', function() {
+      // Check if the input field is empty
+      if (inputElement.value === '') {
+        inputElement.style.backgroundColor = color;
+      } else {
+        inputElement.style.backgroundColor = dimColor;
+      }
+    });
+  }
+}
+
+function resetParamColors() {
+	// Reset "Every Setup" parameters to darker red background
+	everySetupIds.forEach(function(inputId) {
+    const inputElement = document.getElementById(inputId);
+  	inputElement.style.backgroundColor = everySetupRed;
+  });
+  
+  // Reset "Initial Setup" parameters to darker yellow background
+  initialSetupIds.forEach(function(inputId) {
+    const inputElement = document.getElementById(inputId);
+  	inputElement.style.backgroundColor = initialSetupYellow;
+  });
+}
+
+function clearConfig() {
+  var textFieldList = document.querySelector('.text-field-list');
+  var columns = textFieldList.getElementsByClassName('column');
+
+  // Loop through each column
+  Array.from(columns).forEach(function(column) {
+    // Get all input and select elements within the column
+    var params = column.querySelectorAll('input, select');
+
+    // Loop through each parameter and set its value to an empty string
+    Array.from(params).forEach(function(element) {
+      element.value = '';
+    });
+  });
+  
+  resetParamColors();
+}
+
 // Apply red background to "Every Setup" parameters
 everySetupIds.forEach(function(inputId) {
-  applyColorChangeRule(inputId, '#ff000033', '#ff000014');
+  applyParamColorRule(inputId, everySetupRed, everySetupRedDim);
 });
 
 // Apply yellow background to "Initial Setup" parameters
 initialSetupIds.forEach(function(inputId) {
-  applyColorChangeRule(inputId, '#ffff0046', '#ffff0020');
+  applyParamColorRule(inputId, initialSetupYellow, initialSetupYellowDim);
 });
+
+// Clear Button Functionality
+var clearButton = document.getElementById('clear-button');
+clearButton.addEventListener('click', clearConfig); // Clear Config when selected
